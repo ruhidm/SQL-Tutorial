6efbe5feeb5d7a2d6b2c40c7ead0e1098dfab05f
Ruhid <ruhid.mirzayev@gmail.com>
2021-06-26 14:47:49 -0400

    Update HackerRankChallenges.sql

--- HackerRankChallenges.sql
+++ HackerRankChallenges.sql
@@ -1,1 +1,8 @@
 /* Hacker Rank SQL challenges*/
+
+/* Challenge 1*/
+
+/*Query all columns for all American cities in the CITY table with populations larger than 100000. The CountryCode for America is USA.*/
+
+
+SELECT * FROM CITY WHERE CountryCode = 'USA' and POPULATION > 100000
