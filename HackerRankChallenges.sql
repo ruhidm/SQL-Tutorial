4a03f557293e8dda4e1b0d7f55109f4a9c00f2c3
Ruhid <ruhid.mirzayev@gmail.com>
2021-06-28 22:37:59 -0400

    Update HackerRankChallenges.sql

--- HackerRankChallenges.sql
+++ HackerRankChallenges.sql
@@ -44,3 +44,13 @@ SELECT CITY, STATE FROM STATION;
 
 /* Query a list of CITY names from STATION for cities that have an even ID number. Print the results in any order, but exclude duplicates from the answer. */
 
+SELECT CITY FROM STATION WHERE ID % 2 = 0 GROUP BY CITY;
+
+
+
+/* CHALLENGE 7 */
+
+/* Find the difference between the total number of CITY entries in the table and the number of distinct CITY entries in the table.*/
+
+SELECT (COUNT(CITY) - COUNT(DISTINCT(CITY))) FROM STATION;
+
